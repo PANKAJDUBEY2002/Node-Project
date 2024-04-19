802b925affa75286f49f08e5647bb37b53c4cc3e
PANKAJ DUBEY <bt21cse197@iiitn.ac.in>
2024-04-20 02:40:49 +0530

    insight under url

--- Web_server/index.js
+++ Web_server/index.js
@@ -1,7 +1,18 @@
 const http=require("http")
-
+const url=require("url")
 const myServer=http.createServer((req,res)=> {
-    console.log("request arrived")
-    res.end("Server is started")
+    const myUrl=url.parse(req.url,true)
+    switch(myUrl.pathname) {
+        case "/" :
+            res.end("This is Home Page")
+            break
+        case "/about" :
+            res.end('Hii ,${myUrl.query.username}')
+            break
+        default :
+            res.end("404 error")
+    }
+    console.log('${myUrl.pathname}')
+   
 })
-myServer.listen(800,()=> console.log("listening started"))
+myServer.listen(8000,()=> console.log("listening started"))
